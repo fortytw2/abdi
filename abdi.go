4566c33775f010b8fbb6aaf306411428cc348ee1
Ian <fortytw2@gmail.com>
2016-07-22 17:33:23 +0200

    remove dependency on global HMAC key (#5)

--- abdi.go
+++ abdi.go
@@ -3,10 +3,9 @@ package abdi
 import (
 	"crypto/hmac"
 	"crypto/sha256"
-	"unicode/utf8"
-
 	"encoding/base64"
 	"errors"
+	"unicode/utf8"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -16,15 +15,6 @@ var (
 	ErrBlacklistedPassword = errors.New("password is on the blacklist")
 )
 
-// Key is the nonce used for HMAC
-// From Mozilla - " The nonce for the hmac value is designed to be stored on the
-// file system and not in the databases storing the password hashes. In the
-// event of a compromise of hash values due to SQL injection, the nonce will
-// still be an unknown value since it would not be compromised from the file
-// system. This significantly increases the complexity of brute forcing the
-// compromised hashes considering both bcrypt and a large unknown nonce value"
-var Key []byte
-
 // Cost used for the bcrypt hash
 // minimum is 4, maximum is 31
 var Cost = 10
@@ -32,38 +22,44 @@ var Cost = 10
 // MinPasswordLength allowed by abdi
 var MinPasswordLength = 8
 
-// Sign the password first using HMAC, then hash the signed password using salted bcrypt
-func Hash(password string) (string, error) {
+// Hash the password first using HMAC, then hash the signed password using salted bcrypt
+// key is the nonce used for HMAC
+// From Mozilla - " The nonce for the hmac value is designed to be stored on the
+// file system and not in the databases storing the password hashes. In the
+// event of a compromise of hash values due to SQL injection, the nonce will
+// still be an unknown value since it would not be compromised from the file
+// system. This significantly increases the complexity of brute forcing the
+// compromised hashes considering both bcrypt and a large unknown nonce value"
+func Hash(password string, key []byte) (*string, error) {
 	if utf8.RuneCountInString(password) < MinPasswordLength {
-		return "", ErrPasswordTooShort
+		return nil, ErrPasswordTooShort
 	}
 
 	if err := checkBlacklist(password); err != nil {
-		return "", err
+		return nil, err
 	}
 
-	passbyte := []byte(password)
 	// HMAC the password
-	signedPass := sign(passbyte)
+	signedPass := sign([]byte(password), key)
 	// salt + bcrypt the signed password
 	hashedPass, err := hash(signedPass)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	// encode as base64 to return
 	encoded := base64.StdEncoding.EncodeToString(hashedPass)
-	return encoded, nil
+	return &encoded, nil
 }
 
 // Check the password against a hash
-func Check(password string, oldHash string) error {
+func Check(password, oldHash string, key []byte) error {
 	hashed, err := base64.StdEncoding.DecodeString(oldHash)
 	if err != nil {
 		return err
 	}
 
-	signedPass := sign([]byte(password))
+	signedPass := sign([]byte(password), key)
 
 	err = bcrypt.CompareHashAndPassword(hashed, signedPass)
 	if err != nil {
@@ -91,8 +87,8 @@ func hash(pass []byte) ([]byte, error) {
 	return bytes, nil
 }
 
-func sign(pass []byte) []byte {
-	mac := hmac.New(sha256.New, Key)
+func sign(pass, key []byte) []byte {
+	mac := hmac.New(sha256.New, key)
 	mac.Write(pass)
 	expectedMAC := mac.Sum(nil)
 	return expectedMAC
